d507d8d84c738cb9c08bf3cdf5f6f946b7608fcf
msw6468 <c16439@gmail.com>
2024-06-04 00:07:02 +0900

    update LBsim for our server run

--- scripts/merge_hdf5.py
+++ scripts/merge_hdf5.py
@@ -15,28 +15,12 @@ LOAD_DIR = {
     'millet' : '/gallery_millet/chris.kim/data/howto100m/',
     'getty'  : '/gallery_getty/dongjoo.kim/vision/howto370k/',}
 
-META_PART = {
-    'tate' : 0,
-    'moma' : 1,
-    'orsay': 2,
-    'getty': 3,
-    'ai2'  : 4,}
-
 DATA_TYPE = [
-    # For Ours
-    # 'preprocessed_frames',
-    # 'frame_emb',
-    # 'real_text_emb',
-    # 'real_text_sim',
-    # 'synt_text_emb',
-    # 'synt_text',
-    # 'synt_text_sim',
-
     # For LB
     'text_ids',
     'text_emb',
     'clip_emb',
-    'clip_sim',
+    'ciip_sim', # typo
     ]
 
 
@@ -53,29 +37,51 @@ def parse_args():
 def main(args):
     args.debug     = True if args.debug     == "True" else False
 
-    if args.dir_name == 'ai2':
-        if args.merge_type == 'merge_all':
-            """ merge all files based on data_version, data_type
-            """
-            root_dir = os.path.join(LOAD_DIR['ai2'], args.data_version)
-            for data_type in DATA_TYPE:
-                target_file = os.path.join(root_dir, f'{data_type}_total.h5')
-                target_h5  = h5py.File(target_file, 'a')
-                os.chmod(target_h5.filename, mode=0o777)
-                if data_type == 'preprocessed_frames':
-                    source_file_list = glob(os.path.join(root_dir, f'{data_type}_part*.h5py'))
-                else:
-                    source_file_list = glob(os.path.join(root_dir, f'{data_type}_part{args.meta_part}_*_*.h5'))
+    if args.merge_type == 'merge_all':
+        """ merge all files based on data_version, data_type
+        """
+        root_dir = os.path.join(LOAD_DIR['ai2'], args.data_version)
+        for data_type in DATA_TYPE:
+            target_file = os.path.join(root_dir, f'{data_type}_total.h5')
+            target_h5  = h5py.File(target_file, 'a')
+            os.chmod(target_h5.filename, mode=0o777)
+            if data_type == 'preprocessed_frames':
+                source_file_list = glob(os.path.join(root_dir, f'{data_type}_part*.h5py'))
+            else:
+                source_file_list = glob(os.path.join(root_dir, f'{data_type}_part*_*_*.h5'))
 
-                source_file_list.sort()
-                count = 0
-                for source_file in tqdm(source_file_list):
-                    source_h5 = h5py.File(source_file, 'r')
-                    for vid in tqdm(source_h5.keys()):
-                        target_h5.create_dataset(vid, data = source_h5[vid][...])
-                print(count)
-                target_h5.flush()
-                target_h5.close()
+            source_file_list.sort()
+            count = 0
+            for source_file in tqdm(source_file_list):
+                source_h5 = h5py.File(source_file, 'r')
+                for vid in tqdm(source_h5.keys()):
+                    target_h5.create_dataset(vid, data = source_h5[vid][...])
+            print(count)
+            target_h5.flush()
+            target_h5.close()
+
+    elif args.merge_type == 'merge_meta_part':
+        """ merge all files based on data_version, data_type
+        """
+        root_dir = os.path.join(LOAD_DIR['ai2'], args.data_version)
+        for data_type in DATA_TYPE:
+            target_file = os.path.join(root_dir, f'{data_type}_part{args.meta_part}_total.h5')
+            target_h5  = h5py.File(target_file, 'a')
+            os.chmod(target_h5.filename, mode=0o777)
+            if data_type == 'preprocessed_frames':
+                source_file_list = glob(os.path.join(root_dir, f'{data_type}_part*.h5py'))
+            else:
+                source_file_list = glob(os.path.join(root_dir, f'{data_type}_part{args.meta_part}_*_*.h5'))
+
+            source_file_list.sort()
+            count = 0
+            for source_file in tqdm(source_file_list):
+                source_h5 = h5py.File(source_file, 'r')
+                for vid in tqdm(source_h5.keys()):
+                    target_h5.create_dataset(vid, data = source_h5[vid][...])
+            print(count)
+            target_h5.flush()
+            target_h5.close()
 
 
 if __name__ == "__main__":
